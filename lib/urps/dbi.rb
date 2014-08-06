00a1d95ecfacc7a9c9b54e5d9b6000755b303e2e
Joseph Tingsanchali <charnwoot@gmail.com>
2014-08-06 16:12:19 -0500

    modified user class, dbi.rb with more methods for interaction

--- lib/urps/dbi.rb
+++ lib/urps/dbi.rb
@@ -59,9 +59,9 @@ module Arena
 					created_at timestamp NOT NULL DEFAULT current_timestamp
 				)])
 		end
-
-	
-
+	#####################	
+	# Methods for Login #
+	#####################
 		def persist_user(user)
 		  @db.exec_params(%q[
 		    INSERT INTO users (username, password_digest)
@@ -97,6 +97,99 @@ module Arena
 		def build_user(data)
 		  Sesh::User.new(data['username'], data['password_digest'])
 		end
+
+	######################
+	# Methods for Player #
+	######################
+		def save_player(player)
+		end
+
+	#####################
+	# Methods for Match #
+	#####################
+		def persist_match(match)
+			@db.exec_params(%q[
+				INSERT INTO matches (user_id, status, opponent)
+				VALUES ($1, $2, $3);
+				], [match.user_id, match.status, match.opponent])
+		end
+
+		def get_match_by_user_id(user_id)
+			result = @db.exec(%q[
+				SELECT * FROM matches WHERE user_id = '#{user_id}';
+			])
+
+			match_data = result.first
+			if match_data
+				build_match(match_data)
+			else
+				nil
+			end
+		end
+
+		def match_exists?(game)
+			result = @db.exec(%q[
+				SELECT * FROM users WHERE user_id = '#{user_id}';
+			])
+
+			if result.count > 1
+				true
+			else
+				false
+			end
+		end
+
+		def build_match(data)
+			Arena::Match.new(data['user_id'], data['status'], data['opponent'])
+		end
+
+
+	####################
+	# Methods for Game #
+	####################
+	def persist_game()
+		@db.exec_params(%q[
+			INSERT INTO games (match_id, status, opponent, p1move, p2move)
+			VALUES ($1, $2, $3);
+			], [match.user_id, match.status, match.opponent])
+	end
+
+	def get_game_by_match_id(match_id)
+		result = @db.exec(%q[
+			SELECT * FROM games WHERE match_id = '#{match_id}';
+		])
+
+		game_data = result.first
+		if game_data
+			build_match(game_data)
+		else
+			nil
+		end
+	end
+
+	def game_exists?(game.id)
+		result = @db.exec(%q[
+			SELECT * FROM games WHERE game.id = '#{game.lid}';
+		])
+
+		if result.count > 1
+			true
+		else
+			false
+		end
+	end
+
+	def build_game(data)
+		Arena::Game.new(data['user_id'], data['status'], data['opponent'])
+	end
+
+
+
+
+
+
+
+
 
 		def self.dbi
 		  @__db_instance ||= DBI.new
