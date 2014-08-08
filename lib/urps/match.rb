78081398b188b8a9b34d790c7c05273bade6c157
Justin Beuck <jbeuck@yahoo.com>
2014-08-07 20:58:00 -0500

    added information to round and match

--- lib/urps/match.rb
+++ lib/urps/match.rb
@@ -1,14 +1,14 @@
 module  Arena
  class Match
-   attr_accessor :player1, :player2, :player1_win_count, :player2_win_count, :id
+   attr_accessor :player1, :player2, :player1_win_count, :player2_win_count, :match_id
 
-   def initialize(player1, player2=nil, id)
+   def initialize(player1, player2=nil, match_id)
     @player1 = player1
     @player2 = player2
     @player1_win_count = 0
     @player2_win_count = 0
     @total_games = 0
-    @id = id
+    @match_id = match_id
    end
  end
 end
--- lib/urps/round.rb
+++ lib/urps/round.rb
@@ -2,11 +2,14 @@ module Arena
   class Rounds
     attr_accessor :player1move, :player2move, :status, :result
 
-    def initialize(player1move, player2move, status, result)
+    def initialize(player1move, match_id)
       @player1move = player1move
-      @player2move = player2move
+      @player2move = nil
       @status = status
-      @result = result
+      @result = nil
+      @match_id = match_id
     end
+
+    def play(player2move)
   end
 end
